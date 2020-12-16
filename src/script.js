42908c6bb6d5b7608be3f89d990ce73552913796
Theresa26 <62906955+Theresa26@users.noreply.github.com>
2020-12-16 14:17:37 +0100

    updated Weather Forecast

--- src/script.js
+++ src/script.js
@@ -50,20 +50,6 @@ function showSunset(time) {
   return `${sunsetHours}:${sunsetMinutes}`;
 }
 
-// fetch city that was updated in the search engine and update elements using the open weather API
-
-function search(city) {
-  let apiKey = "095c860579f22b3bd8962f914fb1341a";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
-
-  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemp);
-}
-
-function displayCity(event) {
-  event.preventDefault();
-  search(document.getElementById("searchCity").value);
-}
-
 // pull the acutal weather for the location that is being entered
 function showTemp(response) {
   celsiusTemp = Math.round(response.data.main.temp);
@@ -116,15 +102,57 @@ function showTemp(response) {
   icon.setAttribute("alt", response.data.weather[0].description);
 }
 
+// implement forecast to be displayed
+function showForecast(response) {
+  let insertForecast = document.querySelector("#forecast");
+  insertForecast.innerHTML = null;
+
+  for (i = 0; i < 5; i++) {
+    let forecast = response.data.list[i];
+    let forecastTime = showSunrise(response.data.list[i].dt * 1000);
+    let forecastMin = forecast.main.temp_min;
+    let forecastMax = forecast.main.temp_max;
+    let forecastIcon = forecast.weather[0].icon;
+
+    insertForecast.innerHTML += `<div class="row">
+      <div class="col-sm-4 day"> ${forecastTime}</div>
+      <div class="col-sm-4 logo">
+        <img src=" https://openweathermap.org/img/wn/${forecastIcon}@2x.png" />
+      </div>
+  <div class="col-sm-4 temp">${Math.round(forecastMin)}°C / ${Math.round(
+      forecastMax
+    )}°C</div>
+    </div>`;
+  }
+}
+
+// fetch city that was updated in the search engine and update elements using the open weather API
+
+function search(city) {
+  let apiKey = "095c860579f22b3bd8962f914fb1341a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+  let apiUrlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric`;
+
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemp);
+  axios.get(`${apiUrlForecast}&appid=${apiKey}`).then(showForecast);
+}
+
+function displayCity(event) {
+  event.preventDefault();
+  search(document.getElementById("searchCity").value);
+}
+
 // functionality of the current button using geolocation
 function obtainPosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   let apiKey = "095c860579f22b3bd8962f914fb1341a";
   let apiUrlGeo = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric`;
+  let apiUrlGeoForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric`;
 
   // now get weather of the geolocation
   axios.get(`${apiUrlGeo}&appid=${apiKey}`).then(showTemp);
+  axios.get(`${apiUrlGeoForecast}&appid=${apiKey}`).then(showForecast);
 }
 
 function useLocation(event) {
